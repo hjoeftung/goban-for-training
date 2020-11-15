20893a13e62ee7b7d07201a699f8ad81449e0c86
hjoeftung <sto-ton@yandex.ru>
2020-11-16 00:00:07 +0300

    git: added missing files to git

---
+++ goban/routing.py
@@ -0,0 +1,8 @@
+from django.urls import re_path
+
+from . import consumers
+
+
+websocket_urlpatterns = [
+    re_path(r"^game/(?P<game_id>\w+)/", consumers.GameConsumer.as_asgi()),
+]
---
+++ trainingRoom/settings/dev.py
@@ -0,0 +1,4 @@
+from ._base import *
+
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
